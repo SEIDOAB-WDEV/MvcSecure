bd158bd864a6cdd23d0cb3b803b635446fb7c345
Martin <martin_lenart@icloud.com>
2025-12-30 12:00:47 +0100

    update

---
+++ AppRazor/Pages/DataSourceInfo.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services;
+namespace AppRazor.Pages
+{
+	public class DataSourceInfoModel : PageModel
+    {
+        public Models.DTO.GstUsrInfoAllDto Info { get; set; }
+
+        readonly ILogger<DataSourceInfoModel> _logger;
+        readonly IAdminService _service = null;
+
+        public DataSourceInfoModel(ILogger<DataSourceInfoModel> logger, IAdminService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            Info = (await _service.GuestInfoAsync()).Item;
+            return Page();
+        }
+    }
+}
